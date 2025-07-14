c1a8138b69fbe34f3ceb0ad4f5c0d5c5d846a881
BigJeep <128234206+BigJeep@users.noreply.github.com>
2025-07-14 20:56:26 -0300

    zoom in at cursor position
    Guess I did it, but, Jesus, what a mess.

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ const myContextMenu     = document.getElementById('custom-context-menu');
 
 
 // basic constants/parameters
-const boardSize = 15; // better not go beyond 24 for now
+const boardSize = 10; // better not go beyond 24 for now
 const borderLinePx = boardSize > 8 ? 1-0.05*(boardSize-8) : 1; //
 myDebug("tile border size: " + borderLinePx);
 const boardTiles = Array.from(Array(boardSize), (i)=>i=Array(boardSize)); // tile ELEMENTS are stored here, in a 2d array, to be later called by their x-1,y-1
@@ -45,6 +45,8 @@ const playerBoardRect = playerBoard.getBoundingClientRect();
 let mouseX
 let mouseY
 let isMouseInsideViewPort = false
+let boardTranslationX = 0;
+let boardTranslationY = 0;
 const mainViewportRect = mainViewport.getBoundingClientRect();
 
 // Initialization, Listeners, Events
@@ -119,10 +121,6 @@ function resizeBoard () {
     const tiles = document.querySelectorAll('.board-tile')
     const scaleRegex = /scale\(.*?\)/
     
-    // zoomViewport.style.transform = `scale(${scale})`;
-    
-    // myDebug(scaleRegex.test(zoomViewport.style.transform));
-    // myDebug(zoomViewport.style.transform.match(scaleRegex));
     zoomViewport.style.transform = zoomViewport.style.transform.replace(scaleRegex, `scale(${scale})`);
     tiles.forEach(e => {
         e.style.boxShadow = `inset 0 0 0 ${borderSize}px #000`;
@@ -143,22 +141,37 @@ function mouseWheelZoom (event) {
 
     const step = 5
     let newValue = parseInt(zoomSlide.value);
-    if (event.deltaY > 0) {
+    if (event.deltaY > 0) { // ZOOM OUT
         newValue += step;
-    
+        
+        let zoomPercent = 1.5-(zoomSlide.value)/200;
+        boardTranslationY = Math.abs(Math.round(boardTranslationY))-2 != 0 ? boardTranslationY/zoomPercent:0;
+        boardTranslationX = Math.abs(Math.round(boardTranslationX))-3 != 0 ? boardTranslationX/zoomPercent:0;
+        
+        if (zoomSlide.value == 100) {
+            boardTranslationY = 0
+            boardTranslationX = 0
+        }
         
     }
-    else {
+    else { // ZOOM IN
         newValue -= step;
-        
-
+        if (zoomSlide.value > 1) {
+            const zoomX = 1 * Math.abs(mouseX - 960)/20
+            const zoomY = 1 * Math.abs(mouseY - 480)/20
+            boardTranslationX += mouseX > 960 ? - zoomX:zoomX;
+            boardTranslationY += mouseY > 480 ? - zoomY:zoomY;
+            
+            
+        } 
     
     }
     zoomSlide.value = newValue
     resizeBoard ()
     
-    // MOVE TO CURSOR POSITION HERE
-    zoomViewport.style.transform = zoomViewport.style.transform.replace(translateRegex, `translate(${0}px, ${0}px)`);
+    const translateLiteral = `translate(${boardTranslationX}px, ${boardTranslationY}px)`;
+    zoomViewport.style.transform = zoomViewport.style.transform.replace(translateRegex, translateLiteral);
+    
     
 
 
